ddaa18e61ed5ee185b66a7d071ac9e96a2ebce3e
Zenon Trujillo <zenon.t@outlook.com>
2017-04-15 01:39:27 -0700

    applying vector force to player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@
 
 public class Movement : MonoBehaviour {
 
-    public float Speed = .5f;
+    public float Speed = .02f;
 
     Rigidbody2D rb;
 
@@ -12,9 +12,21 @@ public class Movement : MonoBehaviour {
 	
 	void FixedUpdate () {
 
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            //transform.Translate(-touchDeltaPosition.x * Speed, -touchDeltaPosition.y * Speed, 0);
+            rb.AddForce(new Vector2(-touchDeltaPosition.x * Speed, -touchDeltaPosition.y * Speed));
+        }
+
+        /*
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
 
         rb.AddForce(new Vector2(inputH * Speed, inputV * Speed));
-	}
+        */
+    }
 }
